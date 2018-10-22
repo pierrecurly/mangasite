ee37ddd65783e4f4bd7424560a1a48190530999c
Cena <john.pierre.b.cena@accenture.com>
2018-10-22 11:53:01 +0800

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import SideNav from './component/home';
+import NavBar from './component/home';
 
 class App extends Component {
   render() {
     return (
+      <NavBar />
       <SideNav />
     );
   }
---
+++ client/src/components/home/navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navbar, NavItem } from 'react-materialize';
+
+export default
+    <Navbar brand='logo' left>
+        <NavItem href='get-started.html'>Home</NavItem>
+        <NavItem href='components.html'>Manga list</NavItem>
+    </Navbar>;
